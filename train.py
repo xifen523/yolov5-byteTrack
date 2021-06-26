b0efd32c3e69bb5be42effb1eff9776ab14ac208
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-26 16:44:30 +0200

    update

--- train.py
+++ train.py
@@ -213,7 +213,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
     if cuda and RANK == -1 and torch.cuda.device_count() > 1:
         logging.warning('DP not recommended, instead use torch.distributed.run for best DDP Multi-GPU results.\n'
                         'See Multi-GPU Tutorial at https://github.com/ultralytics/yolov5/issues/475 to get started.')
-        #vmodel = torch.nn.DataParallel(model)
+        # model = torch.nn.DataParallel(model)
 
     # SyncBatchNorm
     if opt.sync_bn and cuda and RANK != -1:
