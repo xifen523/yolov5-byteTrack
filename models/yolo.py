fdb0e9e7ef6d77d6be18dc1fee26ec9d9c184479
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-26 13:16:13 +0100

    Update

--- models/yolo.py
+++ models/yolo.py
@@ -90,7 +90,7 @@ class Decouple(nn.Module):
         self.na = na  # number of anchors
         self.nc = nc  # number of classes
         self.a = Conv(c1, c_, 1, s)
-        self.b1, self.b2, self.b3, self.b4 = (Conv(c_, c_, 1, s, autopad(1, p)) for _ in range(4))
+        self.b1, self.b2, self.b3, self.b4 = (Conv(c_, c_, 3, s, autopad(3, p)) for _ in range(4))
         self.c1 = nn.Conv2d(c_, na * 5, (1, 1))  # box, obj outputs  (box, obj, cls...)
         self.c2 = nn.Conv2d(c_, na * nc, (1, 1))  # class outputs
 
