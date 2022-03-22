919a0f01d26ba3fcdf92d08039e276c94f2bf291
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-22 15:16:53 +0100

    Update

--- models/yolo.py
+++ models/yolo.py
@@ -296,7 +296,8 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='yolov5s.yaml', help='model.yaml')
+    # YOLOv5s6: 355 layers, 12626620 parameters, 12626620 gradients, 16.9 GFLOPs
+    parser.add_argument('--cfg', type=str, default='yolov5s6.yaml', help='model.yaml')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--profile', action='store_true', help='profile model speed')
     parser.add_argument('--test', action='store_true', help='test all yolo*.yaml')
