e47d5f1d1c0a122918a97783860de7be63a4ef47
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-25 19:22:16 +0100

    Update

--- models/common.py
+++ models/common.py
@@ -244,7 +244,7 @@ class C2c(nn.Module):
 
 class C3(nn.Module):
     # CSP Bottleneck with 3 convolutions
-    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.75):  # ch_in, ch_out, number, shortcut, groups, expansion
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
