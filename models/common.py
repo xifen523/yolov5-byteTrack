66f9f0ec19795c656713a65a12aa3704d0f2af44
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-25 14:09:15 +0100

    Update

--- models/common.py
+++ models/common.py
@@ -247,7 +247,7 @@ class C3(nn.Module):
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
         self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, c_, e=1.0) for _ in range(n)))
         # self.m = nn.Sequential(*(CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)))
 
     def forward(self, x):
