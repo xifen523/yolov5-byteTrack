93e45b3732a54cae2b363077d4872c91e7f3f65b
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-29 13:17:56 +0200

    Update

--- models/common.py
+++ models/common.py
@@ -96,7 +96,7 @@ class Scale3(nn.Module):
 
 class Scale5(nn.Module):
     # Scale down by 2x in width and height
-    def __init__(self, c1, c2, k=3, s=2):  # ch_in, ch_out, shortcut, groups, expansion
+    def __init__(self, c1, c2, k=5, s=2):  # ch_in, ch_out, shortcut, groups, expansion
         super().__init__()
         c_ = (c1 + c2) // 2
         self.cv1 = Conv(c1, c_, (1, k), (1, s))
@@ -261,6 +261,24 @@ class C3(nn.Module):
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
         self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
+        # self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
+        self.m = nn.Sequential(*(CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)))
+
+    def forward(self, x):
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
+
+
+from utils.activations import FReLU
+
+
+class C3F(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1, act=FReLU(c2))
         # self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
         self.m = nn.Sequential(*(CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)))
 
