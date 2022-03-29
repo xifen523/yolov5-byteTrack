66a30708d93906b84d44852db058946ac4745771
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-29 13:46:54 +0200

    Update

--- models/common.py
+++ models/common.py
@@ -288,14 +288,31 @@ class C3c(nn.Module):
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
+        c__ = int(c1 * 0.75)
         self.cv1 = Conv(c1, c_, 1, 1)
-        self.cv2 = Conv(c_, c_, 1, 1)
-        self.cv3 = Conv(2 * c_, c2, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(c1, c__, 1)  # optional act=FReLU(c2)
         self.m = nn.Sequential(*(CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)))
 
     def forward(self, x):
-        y = self.cv2(self.cv1(x))
-        return self.cv3(torch.cat((self.m(y), x), 1))
+        x = self.cv3(x)
+        return torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1)
+
+
+class C3d(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        c__ = int(c1 * 0.75)
+        self.cv1 = Conv(c__, c_, 1, 1)
+        self.cv2 = Conv(c__, c_, 1, 1)
+        self.cv3 = Conv(c1, c__, 1)  # optional act=FReLU(c2)
+        self.m = nn.Sequential(*(CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)))
+
+    def forward(self, x):
+        x = self.cv3(x)
+        return torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1)
 
 
 class C3(nn.Module):
