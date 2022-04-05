4ba3c16b1956921e9dc4831fc81164e39b6fb2c1
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-05 18:41:39 +0200

    Update

--- dl365.py
+++ dl365.py
@@ -15,9 +15,12 @@ for split, patches in [('train', 50 + 1), ('val', 43 + 1)]:
     # Download
     url = f"https://dorc.ks3-cn-beijing.ksyun.com/data-set/2020Objects365%E6%95%B0%E6%8D%AE%E9%9B%86/{split}/"
     if split == 'train':
-        download([f'{url}zhiyuan_objv2_{split}.tar.gz'], dir=dir, delete=False)  # annotations json
-        download([f'{url}patch{i}.tar.gz' for i in range(patches)], dir=images, curl=True, delete=False, threads=8)
+        # download([f'{url}zhiyuan_objv2_{split}.tar.gz'], dir=dir, delete=False)  # annotations json
+        download([f'{url}patch{i}.tar.gz' for i in range(patches)], dir=images, curl=True, delete=False, threads=1,
+                 unzip=False)
     elif split == 'val':
         download([f'{url}zhiyuan_objv2_{split}.json'], dir=dir, delete=False)  # annotations json
-        download([f'{url}images/v1/patch{i}.tar.gz' for i in range(15 + 1)], dir=images, curl=True, delete=False, threads=8)
-        download([f'{url}images/v2/patch{i}.tar.gz' for i in range(16, patches)], dir=images, curl=True, delete=False, threads=8)
+        download([f'{url}images/v1/patch{i}.tar.gz' for i in range(15 + 1)], dir=images, curl=True, delete=False,
+                 threads=1, unzip=False)
+        download([f'{url}images/v2/patch{i}.tar.gz' for i in range(16, patches)], dir=images, curl=True, delete=False,
+                 threads=1, unzip=False)
