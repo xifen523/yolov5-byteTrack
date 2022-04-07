e0e5362b0c64b9809043aadbd6ac8022abc5990f
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-07 17:52:53 +0200

    Update

--- utils/activations.py
+++ utils/activations.py
@@ -55,10 +55,10 @@ class FReLU(nn.Module):
     def __init__(self, c1, k=3):  # ch_in, kernel
         super().__init__()
         self.conv = nn.Conv2d(c1, c1, k, 1, 1, groups=c1, bias=False)
-        self.bn = nn.BatchNorm2d(c1)
+        #self.bn = nn.BatchNorm2d(c1)
 
     def forward(self, x):
-        return torch.max(x, self.bn(self.conv(x)))
+        return self.conv(x) # torch.max(x, self.bn(self.conv(x)))
 
 
 class AconC(nn.Module):
